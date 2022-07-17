c2fb08aa46f0069c8daa7e3bdafae4ab53e3baad
yonghk423 <yonghk423423@gmail.com>
2022-07-17 18:00:02 +0900

    pf

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import home from "../assets/home.png"
+import play from "../assets/play.png"; 
+// import { homeAnimation, homeInfoAnimation } from "animations";
+import Navbar from './Navbar';
+
+const Section = styled.section`
+  background-image: url(${home});
+  background-size: cover;
+  min-height: 100vh;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: relative;
+`;
 
 const Home = () => {
     return (
-        <></>
+        <Section>
+            <Navbar/>
+        </Section>
+        
     )
 }
 
--- src/components/useScroll.tsx
+++ src/components/useScroll.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import { useInView } from "react-intersection-observer";
+import { useAnimation } from "framer-motion";
 
-const useScroll = () => {
-    return (
-        <></>
-    )
-}
+export const useScroll = (thresh = 0.1) => {
+  const controls = useAnimation();
+  const [element, view] = useInView({ threshold: thresh });
 
-export default useScroll;
+  if (view) {
+    controls.start("show");
+  } else {
+    controls.start("hidden");
+  }
+
+  return [element, controls];
+};
