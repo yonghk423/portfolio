0196e3edff8ec2066bf3d3499344016378a4b60b
yonghk423 <yonghk423423@gmail.com>
2022-07-17 16:00:12 +0900

    pf

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { motion } from "framer-motion";
+import ScrollToTop from './components/ScrollToTop';
+import Home from './components/Home';
+import Services from './components/Services';
+import Portfolio from './components/Portfolio';
+import Milestones from './components/Milestones';
+import Blog from './components/Blog';
+import Video from './components/Video';
+import Pricing from './components/Pricing';
+import Testimonials from './components/Testimonials';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div initial="hidden" animate="show">
+      <ScrollToTop/>
+      <Home/>
+      <Services/>
+      <Portfolio/>
+      <Milestones/>
+      <Blog/>
+      <Video/>
+      <Pricing/>
+      <Testimonials/>
+      <Skills/>
+      <Contact/>
+      <Footer/>
+    </motion.div>
   );
 }
 
